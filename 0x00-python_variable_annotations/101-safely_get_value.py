0547062cade15738838bd4553f488d58c5c27a24
deelight-del <akingbenidavidoyin@gmail.com>
2024-04-18 16:59:27 +0000

    fix task 1

--- 0x00-python_variable_annotations/101-safely_get_value.py
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -4,7 +4,7 @@ function annotation completion"""
 from typing import Sequence, Union, Mapping, TypeVar, Any
 
 T = TypeVar('T')
-def safely_get_value(dct: Mapping[Any, Any], key: Any, default: Union[~T, None] = None) -> Union[Any, ~T]:
+def safely_get_value(dct: Mapping, key: Any, default: Union[T, None] = None) -> Union[Any, T]:
     """Function implementation of safely_get_value"""
     if key in dct:
         return dct[key]
