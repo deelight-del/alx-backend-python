beb1066ee4b0cecabeb4658e9f0c642dd9d6a2b7
deelight-del <akingbenidavidoyin@gmail.com>
2024-04-18 15:22:51 +0000

    Task 100: fix mypy issues

--- 0x00-python_variable_annotations/100-safe_first_element.py
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
 """Module that implements Basic Annotations and
 function annotation completion"""
-from typing import Any, Optional
+from typing import Any, Sequence, Union
 
 
-def safe_first_element(lst: Any) -> Optional[Any]:
+def safe_first_element(lst: Sequence[Any]) -> Union[Any, None]:
     """Function implementation of safe_first_element"""
     if lst:
         return lst[0]
