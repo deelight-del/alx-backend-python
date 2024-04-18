612cca21e0eccf08875817a29f7af72fa3bee21a
deelight-del <akingbenidavidoyin@gmail.com>
2024-04-18 14:25:58 +0000

    task 2

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""Module that implements Basic Annotations and
+floor function"""
+
+
+def floor(n: float) -> int:
+    """Implementation of the floor funciton"""
+    return int(n)
