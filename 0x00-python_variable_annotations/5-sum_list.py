591b6e7e2197c47f0991421a0e5fb860304c36f1
deelight-del <akingbenidavidoyin@gmail.com>
2024-04-18 14:34:16 +0000

    fix mypy issues

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 """Module that implements Basic Annotations and
 function definition of sum_list"""
+from typing import List
 
 
-def sum_list(input_list: list[float]) -> float:
+def sum_list(input_list: List[float]) -> float:
     return sum(input_list)
