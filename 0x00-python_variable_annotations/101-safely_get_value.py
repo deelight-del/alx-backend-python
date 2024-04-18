ccf1f60d0e448ffbed1a58fba79741442846126d
deelight-del <akingbenidavidoyin@gmail.com>
2024-04-18 16:28:37 +0000

    Task 102

--- 0x00-python_variable_annotations/101-safely_get_value.py
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
 """Module that implements Basic Annotations and
 function annotation completion"""
-from typing import Any, Sequence, Union, Mapping, TypeVar
+from typing import Sequence, Union, Mapping, TypeVar, Any
 
 T = TypeVar('T')
-def safely_get_value(dct: Mapping[Any, T], key: Any, default: Union[T, None] = None) -> Union[Any, T]:
+def safely_get_value(dct: Mapping[Any, Any], key: Any, default: Union[~T, None] = None) -> Union[Any, ~T]:
     """Function implementation of safely_get_value"""
     if key in dct:
         return dct[key]
---
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""Module that implements Basic Annotations and
+function annotation completion"""
+from typing import List, TypeVar, Any, Tuple
+
+
+def zoom_array(lst: Tuple, factor: int = 2) -> List[Any]:
+    zoomed_in: List = [
+        item for item in lst
+        for i in range(factor)
+    ]
+    return zoomed_in
+
+
+array = (12, 72, 91)
+
+zoom_2x = zoom_array(array)
+
+zoom_3x = zoom_array(array, 3)
