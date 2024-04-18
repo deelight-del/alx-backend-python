0d70690b113f9726c58e49e0c4e9a86a195f2826
deelight-del <akingbenidavidoyin@gmail.com>
2024-04-18 14:33:05 +0000

    Task 5

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""Module that implements Basic Annotations and
+function definition of sum_list"""
+
+
+def sum_list(input_list: list[float]) -> float:
+    return sum(input_list)
