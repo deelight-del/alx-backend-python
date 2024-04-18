b42b64c70593aad78f568330913ec0e7f4c3c3b4
deelight-del <akingbenidavidoyin@gmail.com>
2024-04-18 14:18:12 +0000

    Task 1

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""Module that implements Basic Annotations and
+concat Function"""
+
+
+def concat(str1: str, str2: str):
+    """Concat function implementationn"""
+    return str1 + str2
