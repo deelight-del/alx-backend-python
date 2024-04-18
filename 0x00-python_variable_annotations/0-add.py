cd0150b3bf597f4ffd79f60206a7026166113342
deelight-del <akingbenidavidoyin@gmail.com>
2024-04-18 14:10:54 +0000

    Task 0

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""In this module, we implement add function
+that implements using python annotation"""
+
+
+def add(a: float, b: float) -> float:
+    """Function implementation of the add funciton"""
+    return a + b
---
+++ 0x00-python_variable_annotations/README.md
@@ -0,0 +1,4 @@
+# Python Annotations.
+
+And after some week in the Javascript (and TypeScript) wilderness,
+we have finally returned to Python and Python Annotations.
