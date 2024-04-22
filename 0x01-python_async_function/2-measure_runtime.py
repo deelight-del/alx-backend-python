bed2f4e10ddaa75cee9d4017f838b5ec286869ad
deelight-del <akingbenidavidoyin@gmail.com>
2024-04-22 16:13:35 +0000

    Task 2 fix: malloc issues

--- 0x01-python_async_function/2-measure_runtime.py
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -11,9 +11,9 @@ import time
 wait_n = __import__('1-concurrent_coroutines').wait_n
 
 
-async def measure_time(n: int, max_delay: int) -> float:
+def measure_time(n: int, max_delay: int) -> float:
     """The implementation of the measure_time function"""
     current_s = time.perf_counter()
-    await wait_n(n, max_delay)
+    wait_n(n, max_delay)
     new_s = time.perf_counter() - current_s
     return new_s/n
