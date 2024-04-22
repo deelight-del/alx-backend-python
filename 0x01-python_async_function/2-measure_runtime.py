35a632601e94d5aba486a54f5fa501572e6d6217
deelight-del <akingbenidavidoyin@gmail.com>
2024-04-22 16:11:50 +0000

    task 2: fix await

--- 0x01-python_async_function/2-measure_runtime.py
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -14,6 +14,6 @@ wait_n = __import__('1-concurrent_coroutines').wait_n
 async def measure_time(n: int, max_delay: int) -> float:
     """The implementation of the measure_time function"""
     current_s = time.perf_counter()
-    wait_n(n, max_delay)
+    await wait_n(n, max_delay)
     new_s = time.perf_counter() - current_s
     return new_s/n
