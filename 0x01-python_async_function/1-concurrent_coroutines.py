0ed20355d105d28c0b6291bd54e078cdc5091422
deelight-del <akingbenidavidoyin@gmail.com>
2024-04-22 15:48:37 +0000

    fix pycode

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -12,5 +12,9 @@ wait_random = __import__('0-basic_async_syntax').wait_random
 
 async def wait_n(n: int, max_delay: int) -> List[float]:
     """Function implementation of wait_n"""
-    random_values = await asyncio.gather(*[wait_random(max_delay) for _ in range(n)])
+    random_values = await asyncio.gather(
+        *[
+            wait_random(max_delay)
+            for _ in range(n)
+        ])
     return sorted(random_values)
