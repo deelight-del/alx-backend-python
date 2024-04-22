3a3c48307264156e2cc835d1fbbfc9538da75240
deelight-del <akingbenidavidoyin@gmail.com>
2024-04-22 16:07:20 +0000

    task 2

---
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""In this module we explore the basics of
+the async syntax"""
+
+
+import asyncio
+from typing import List
+import time
+
+
+wait_n = __import__('1-concurrent_coroutines').wait_n
+
+
+async def measure_time(n: int, max_delay: int) -> float:
+    """The implementation of the measure_time function"""
+    current_s = time.perf_counter()
+    wait_n(n, max_delay)
+    new_s = time.perf_counter() - current_s
+    return new_s/n
