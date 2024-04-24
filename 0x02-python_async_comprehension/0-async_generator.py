8ca7326846c4131d86be6516f1a84e91ab6c419a
deelight-del <akingbenidavidoyin@gmail.com>
2024-04-24 10:40:40 +0000

    attempt again to change to Generator

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -3,11 +3,11 @@
 
 
 import random
-from typing import AsyncGenerator
+from typing import Generator
 import asyncio
 
 
-async def async_generator() -> AsyncGenerator[float, None]:
+async def async_generator() -> Generator[float, None, None]:
     """implementation of the async_genrator function"""
     for _ in range(10):
         await asyncio.sleep(1)
